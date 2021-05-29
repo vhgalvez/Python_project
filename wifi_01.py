29e20bf7201a5baf9a5e1173f31c9321dc8ab69a
Victor Sastoque <vhgalvez@gmail.com>
2021-05-29 10:40:03 +0200

    Create wifi_01.py

---
+++ wifi_01.py
@@ -0,0 +1,23 @@
+print('Bienvenido al sistema de ubicación para zonas públicas WIFI')
+nombre_de_usuario= 51628
+contrasena= 82615
+a=int(input(''))
+if a == nombre_de_usuario: 
+    pass 
+else: 
+    print('Error')
+    exit(0)
+b=int(input(''))
+if b == contrasena: 
+    pass 
+else: 
+    print('Error')
+    exit(0)
+c=628
+d=((6-1)+5)-8
+print('628 + 2 =')
+e=int(input(''))
+if e == c + d: 
+    print('Sesión iniciada') 
+else: 
+    print('Error')
